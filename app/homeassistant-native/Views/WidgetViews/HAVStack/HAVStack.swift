3eb6879361258db86cb6c802a1c4a24d9a3b1c84
Charles Vu <753278+CharlesVu@users.noreply.github.com>
2024-04-11 00:24:28 +0100

    Make HAVStack reactive to changes.

--- app/homeassistant-native/Views/WidgetViews/HAVStack/HAVStack.swift
+++ app/homeassistant-native/Views/WidgetViews/HAVStack/HAVStack.swift
@@ -6,18 +6,26 @@ import SwiftUI
 class HAVStackViewModel: ObservableObject {
     @Injected(\.displayableStore) var displayableStore
     var configuration: VStackConfiguration!
-    var tokens: [NotificationToken] = []
+    var configurationObserverToken: NotificationToken?
     @Published var subViews = [HAViewType]()
 
     init(displayableModelObjectID: String) {
         configuration = displayableStore.vStackConfiguration(displayableModelObjectID: displayableModelObjectID)
         Task {
+            await observeConfiguration()
             await mapSubViews()
         }
     }
 
     @MainActor
-    func mapSubViews() async {
+    func observeConfiguration() {
+        configurationObserverToken = configuration.observe { [weak self] change in
+            self?.mapSubViews()
+        }
+    }
+
+    @MainActor
+    func mapSubViews() {
         subViews = []
         for element in configuration.children {
             if let destination = HAViewBuilder().map(model: element) {
